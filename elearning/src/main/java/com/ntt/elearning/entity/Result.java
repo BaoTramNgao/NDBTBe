546f9d62b3ef3bb736a1ef4790168c4c76a6f79b
tawn <huutankg2005@gmail.com>
2024-10-15 21:50:30 +0700

    add Result

---
+++ elearning/src/main/java/com/ntt/elearning/entity/Result.java
@@ -0,0 +1,30 @@
+package com.ntt.elearning.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Result {
+    @Id
+    String id;
+
+    @ManyToMany
+    Set<User> userId;
+
+    @ManyToMany
+    Set<Course> courseId;
+
+    double score;
+}
