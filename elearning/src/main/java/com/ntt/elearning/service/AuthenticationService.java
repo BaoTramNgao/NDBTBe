482fe9b001121e18bb3ea973185633b252138ad3
tawn <huutankg2005@gmail.com>
2024-10-17 00:59:42 +0700

    almost done security

---
+++ elearning/src/main/java/com/ntt/elearning/service/AuthenticationService.java
@@ -0,0 +1,107 @@
+package com.ntt.elearning.service;
+
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.StringJoiner;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.ntt.elearning.dto.request.AuthenticationRequest;
+import com.ntt.elearning.dto.request.IntrospectRequest;
+import com.ntt.elearning.dto.response.AuthenticationResponse;
+import com.ntt.elearning.dto.response.IntrospectResponse;
+import com.ntt.elearning.entity.User;
+import com.ntt.elearning.exception.AppException;
+import com.ntt.elearning.exception.ErrorCode;
+import com.ntt.elearning.repository.UserRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class AuthenticationService {
+    UserRepository userRepository;
+
+    @NonFinal
+    protected static final String SINGER_KEY = "sDd3tkGGcBWFeKUZsqBgXGPx/dkcXehI8WcYobvUW1T7tLqgVsFHXDPotVIK2TL/";
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        var user = userRepository
+                .findByUsername(request.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        PasswordEncoder encoder = new BCryptPasswordEncoder(10);
+        boolean authenticated = encoder.matches(request.getPassword(), user.getPassword());
+
+        if (!authenticated) throw new AppException(ErrorCode.UNAUTHENTICATED);
+
+        var token = generateToken(user);
+        return AuthenticationResponse.builder().token(token).authenticated(true).build();
+    }
+
+    private String generateToken(User user) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject(user.getUsername())
+                .issuer("ntt.com")
+                .issueTime(new Date())
+                .claim("scope", buildScope(user))
+                .expirationTime(
+                        new Date(Instant.now().plus(1, ChronoUnit.MONTHS).toEpochMilli()))
+                .build();
+
+        Payload payload = new Payload(claimsSet.toJSONObject());
+        JWSObject jwtObject = new JWSObject(header, payload);
+
+        try {
+            jwtObject.sign(new MACSigner(SINGER_KEY.getBytes()));
+
+            return jwtObject.serialize();
+        } catch (JOSEException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public IntrospectResponse introspect(IntrospectRequest request) throws JOSEException, ParseException {
+        var token = request.getToken();
+        JWSVerifier verifier = new MACVerifier(SINGER_KEY.getBytes());
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expityTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        var verified = signedJWT.verify(verifier);
+
+        return IntrospectResponse.builder()
+                .valid(verified && expityTime.after(new Date()))
+                .build();
+    }
+
+    private String buildScope(User user) {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if (!CollectionUtils.isEmpty(user.getRoles())) {
+            user.getRoles().forEach(role -> {
+                stringJoiner.add("ROLE_" + role.getName());
+                if (!CollectionUtils.isEmpty(role.getPermissions()))
+                    role.getPermissions().forEach(permission -> stringJoiner.add(permission.getName()));
+            });
+        }
+        return stringJoiner.toString();
+    }
+}
