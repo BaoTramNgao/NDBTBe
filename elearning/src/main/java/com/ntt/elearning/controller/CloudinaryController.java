fdc38f6604f718af3bd23f833969c7c27f05ba20
tawn <huutankg2005@gmail.com>
2024-11-07 21:55:04 +0700

    ..

--- elearning/src/main/java/com/ntt/elearning/controller/CloudinaryController.java
+++ elearning/src/main/java/com/ntt/elearning/controller/CloudinaryController.java
@@ -1,16 +1,14 @@
 package com.ntt.elearning.controller;
 
-import java.io.IOException;
 import java.util.Map;
 
-import com.ntt.elearning.service.CloudinaryService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
+import com.ntt.elearning.service.CloudinaryService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,8 +19,8 @@ public class CloudinaryController {
 
     private final CloudinaryService cloudinaryService;
 
-    @PostMapping
-    public ResponseEntity<Map> uploadImage(@RequestParam("image")MultipartFile file){
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Map> uploadImage(@ModelAttribute MultipartFile file) {
         Map data = this.cloudinaryService.upload(file);
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
